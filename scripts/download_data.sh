2b966a4c5953bedaa43735b2f44d3ba96dfe401d
medine.balci <medine.balci@uzh.ch>
2025-03-28 15:02:43 +0100

    changed wicked to little women and preprocessed that

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -10,22 +10,22 @@ tools="$base/tools"
 mkdir -p "$data"
 
 # Link default training data for easier access
-mkdir -p "$data/wicked"
+mkdir -p "$data/littlewomen"
 
 
 # Preprocess Wicked
-mkdir -p "$data/wicked/raw"
+mkdir -p "$data/littlewomen/raw"
 
-if [[ -f "$data/wicked/raw/wicked.txt" ]]; then
-    cat "$data/wicked/raw/wicked.txt" | python "$base/scripts/preprocess_raw.py" > "$data/wicked/raw/wicked.cleaned.txt"
+if [[ -f "$data/littlewomen/raw/littlewomen.txt" ]]; then
+    cat "$data/littlewomen/raw/littlewomen.txt" | python "$base/scripts/preprocess_raw.py" > "$data/littlewomen/raw/littlewomen.cleaned.txt"
 
     # Tokenize and preprocess
-    cat "$data/wicked/raw/wicked.cleaned.txt" | python "$base/scripts/preprocess.py" --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > "$data/littlewomen/raw/littlewomen.preprocessed.txt"
+    cat "$data/littlewomen/raw/littlewomen.cleaned.txt" | python "$base/scripts/preprocess.py" --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > "$data/littlewomen/raw/littlewomen.preprocessed.txt"
 
     # Split into train, valid, and test
-    head -n 440 "$data/wicked/raw/wicked.preprocessed.txt" | tail -n 400 > "$data/wicked/valid.txt"
-    head -n 840 "$data/wicked/raw/wicked.preprocessed.txt" | tail -n 400 > "$data/wicked/test.txt"
-    tail -n 3075 "$data/wicked/raw/wicked.preprocessed.txt" | head -n 2955 > "$data/wicked/train.txt"
+    head -n 440 "$data/littlewomen/raw/littlewomen.preprocessed.txt" | tail -n 400 > "$data/littlewomen/valid.txt"
+    head -n 840 "$data/littlewomen/raw/littlewomen.preprocessed.txt" | tail -n 400 > "$data/littlewomen/test.txt"
+    tail -n 3075 "$data/littlewomen/raw/littlewomen.preprocessed.txt" | head -n 2955 > "$data/littlewomen/train.txt"
 else
-    echo "Error: The file wicked.txt does not exist in $data/wicked/raw/"
+    echo "Error: The file littlewomen.txt does not exist in $data/littlewomen/raw/"
 fi
